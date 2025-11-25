b4af0a0c5c86c541a426cc46b1cd24507b0a012f
Sawyer06 <sawblade0@outlook.com>
2025-11-24 20:26:29 -0500

    Confirmation/Receipt page calculations

--- project/webapp/payment.py
+++ project/webapp/payment.py
@@ -1,8 +1,12 @@
+import datetime
+
 from flask import (
     Blueprint, render_template, request,
-    redirect, url_for, flash, session
+    redirect, url_for, flash, session, g
 )
 from .auth import login_required
+from .models import ShoppingCart, ShoppingCartItem, InventoryItem, Logistics
+from .db import db
 
 bp = Blueprint("payment", __name__, url_prefix="/payment")
 
@@ -10,7 +14,8 @@ bp = Blueprint("payment", __name__, url_prefix="/payment")
 @bp.route("/", methods=["GET", "POST"])
 @login_required
 def pay():
-    card = expiration = cvv = shipping = ""
+    logistics = Logistics.query.first()
+
     if request.method == "POST":
         # Shipping information.
         address = request.form.get("address", "").strip()
@@ -50,22 +55,80 @@ def pay():
             error = "Shipping option selection is required."
 
         if error is None: # All required fields filled. Can confirm order.
+            # Determine cost of shipping.
+            shipping_cost = logistics.ground_shipping
+            if shipping == "overnight":
+                shipping_cost = logistics.overnight_shipping
+            elif shipping == "three_day":
+                shipping_cost = logistics.three_day_shipping
+
+            session["shipping_info"] = {"address":address,"zip_code":zip_code,
+                                        "city":city,"state":state,"phone":phone,
+                                        "card":card,"shipping_cost":shipping_cost}
+
             return redirect(url_for("payment.confirm"))
 
         flash(error)
 
-    return render_template("payment/pay.html")
+    return render_template("payment/pay.html", logistics=logistics)
 
 # Confirm order button is pressed on pay now page.
 @bp.route("/confirm", methods=["GET", "POST"])
 @login_required
 def confirm():
-    if request.method == "POST":
-        return redirect(url_for("payment.receipt"))
+    cart = ShoppingCart.query.filter_by(user_id=g.user.id, is_checked_out=False).first()
+    if cart is None:
+        return redirect(url_for("home.index"))
 
-    return render_template("payment/confirm.html")
+    logistics = Logistics.query.first()
+    tax = cart.sub_total * logistics.tax
+    shipping_info = session.get("shipping_info", {})
+    shipping_cost = shipping_info.get("shipping_cost")
+    total = cart.sub_total + shipping_cost + tax
 
-@bp.route("/receipt/<int:cart_id>", methods=["GET"])
+    cart_items = ShoppingCartItem.query.filter_by(shopping_cart_id=cart.id).all()
+    items = get_items(cart_items)
+
+    if request.method == "POST": # Payment confirmed.
+        action = request.form.get("action")
+
+        if action == "complete": # Pressed complete order button.
+            cart.tax = tax
+            cart.total_cost = total
+            cart.is_checked_out = True
+            cart.date_checked_out = datetime.datetime.now()
+
+            db.session.commit()
+
+            return redirect(url_for("payment.receipt", cart_id=cart.id))
+        elif action == "cancel": # Pressed cancel order button.
+            return redirect(url_for("home.index"))
+
+    return render_template("payment/confirm.html",items=items)
+
+@bp.route("/receipt/<int:cart_id>", methods=["GET", "POST"])
 @login_required
-def view_receipt():
-    return
+def view_receipt(cart_id):
+    cart = ShoppingCart.query.filter_by(user_id=g.user.id, is_checked_out=False).first()
+    if cart is None:
+        return redirect(url_for("home.index"))
+
+    if not cart.is_checked_out:
+        return redirect(url_for("home.index"))
+
+    cart_items = ShoppingCartItem.query.filter_by(shopping_cart_id=cart.id).all()
+    items = get_items(cart_items)
+
+    if request.method == "POST":
+        return redirect(url_for("home"))
+
+    return render_template("payment/receipt.html", cart_id=cart_id, items=items)
+
+# Get the corresponding inventory items from cart items.
+def get_items(cart_items):
+    items = []
+    for cart_item in cart_items:
+        item = InventoryItem.query.get(cart_item.inventory_item_id)
+        if item:
+            items.append(cart_item)
+    return items
