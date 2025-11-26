c7acc2fd3591b1f3db9e3f5f91b2cd5fcf51ba82
luke <onegqluke@icloud.com>
2025-11-26 17:28:23 -0500

    Shopping cart calculates total correctly on run

--- project/webapp/home.py
+++ project/webapp/home.py
@@ -13,11 +13,19 @@ bp = Blueprint("home", __name__)
 @login_required
 def index():
     # Load all products
-    user_shopping_cart_uuid = ShoppingCart.query.filter_by(user_id=g.user.id, is_checked_out=False).first()
-    users_shopping_cart_length = len(ShoppingCartItem.query.filter_by(shopping_cart_id=user_shopping_cart_uuid).all())
-    print(users_shopping_cart_length)
+    user_shopping_cart_uuid = ShoppingCart.query.filter_by(user_id=g.user.id, is_checked_out=False).first().id
+    users_shopping_cart_items = ShoppingCartItem.query.filter_by(shopping_cart_id=user_shopping_cart_uuid).all()
+    
+    total = 0
+    for cart_item in users_shopping_cart_items:
+        total += InventoryItem.query.filter_by(id=cart_item.inventory_item_id).first().cost
+    
+    total /= 100.0
 
-    products = InventoryItem.query.all()
-    return render_template("home.html", products=products, item_count=users_shopping_cart_length)
+    users_shopping_cart_length = len(users_shopping_cart_items)
+
+
+    products = InventoryItem.query.filter_by(is_available=True).all()
+    return render_template("home.html", products=products, item_count=users_shopping_cart_length, sub_total=total)
 
 
