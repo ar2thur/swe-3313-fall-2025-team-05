36a575c198dc2d846f2a30e8dd4f64b79f6bad9d
Joshua Reyes <joshuadv94@gmail.com>
2025-11-28 23:54:59 -0500

    Shopping cart calculates total correctly on run

--- project/webapp/home.py
+++ project/webapp/home.py
@@ -14,10 +14,15 @@ bp = Blueprint("home", __name__)
 def index():
     # Load all products
     user_shopping_cart_uuid = ShoppingCart.query.filter_by(user_id=g.user.id, is_checked_out=False).first()
-    users_shopping_cart_length = len(ShoppingCartItem.query.filter_by(shopping_cart_id=user_shopping_cart_uuid).all())
-    print(users_shopping_cart_length)
+    users_shopping_cart_items = ShoppingCartItem.query.filter_by(shopping_cart_id=user_shopping_cart_uuid).all()
+    
+    total = 0
+    for cart_item in users_shopping_cart_items:
+        total += InventoryItem.query.filter_by(id=cart_item.inventory_item_id).first().cost
 
-    products = InventoryItem.query.all()
-    return render_template("home.html", products=products, item_count=users_shopping_cart_length)
+    total /= 100  # Convert cents to dollars
 
+    user_shopping_cart_length = len(users_shopping_cart_items)
 
+    products = InventoryItem.query.filter_by(is_available=True).all()
+    return render_template("home.html", products=products, item_count=user_shopping_cart_length, sub_total=total)
