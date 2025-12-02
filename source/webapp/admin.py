7ceabecfc3a1151b0160dc684bac86ab5eb93f04
luke <onegqluke@icloud.com>
2025-12-02 12:00:46 -0500

    Dashboard loads accurate data from database

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -3,7 +3,7 @@ from flask import (
     redirect, url_for, flash, session, g
 )
 
-from webapp.auth import login_required, admin_required
+from webapp.auth import admin_required
 from webapp.db import db
 from webapp.models import InventoryItem, User, ShoppingCart, ShoppingCartItem
 
@@ -12,7 +12,27 @@ bp = Blueprint("admin", __name__)
 @bp.route("/dashboard")
 @admin_required
 def dashboard():
-    return render_template("admin/dashboard.html")
 
+    data = get_dashboard_data()
+
+    return render_template("admin/dashboard.html",
+                           nusers=data[0],
+                           ncarts=data[1],
+                           nsales=data[2],
+                           nrevenue=data[3],
+                           recent_sales=data[4]
+                           )
+
+def get_dashboard_data():
+
+    amount_of_users = db.session.query(User).count()
+    items_in_cart = db.session.query(ShoppingCartItem).count()
+    bought_carts = ShoppingCart.query.filter_by(is_checked_out=True).all()
+    sales_made = len(bought_carts)
+    total_revenue = sum(cart.total_cost for cart in bought_carts)
+
+
+
+    return (amount_of_users, items_in_cart, sales_made, total_revenue, bought_carts)
 
 
