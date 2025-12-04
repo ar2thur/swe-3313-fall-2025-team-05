536db96b9f4a6abab53145c656b0e4e3f15718e1
Joshua Reyes <joshuadv94@gmail.com>
2025-12-04 17:45:12 -0500

    Introduced admin routes

---
+++ source/webapp/admin.py
@@ -0,0 +1,14 @@
+from flask import (
+    Blueprint, render_template, request,
+    redirect, url_for, flash, session, g
+)
+
+from webapp.auth import login_required, admin_required
+from webapp.db import db
+from webapp.models import InventoryItem, User, ShoppingCart, ShoppingCartItem
+
+bp = Blueprint("admin", __name__)
+
+@bp.route("/dashboard")
+@admin_required
+# def dashboard():
